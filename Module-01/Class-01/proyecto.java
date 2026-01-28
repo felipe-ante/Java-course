2f3f8ea97eb13192b2471010b9213c0d99940c5d
Felipe Ante <Felpe.Ante@gmail.com>
2026-01-28 17:24:32 -0500

    feat: se crea el primer programa

---
+++ Module-01/Class-01/proyecto.java
@@ -0,0 +1,8 @@
+public class proyecto{
+    public static void main(String[] args) 
+    {
+        //este es un programa le da la bienvenida  a cesar  //
+        System.out.println("CESAR, Bienvenido al curso de programacion
+        en Java!!!");
+    }
+}
