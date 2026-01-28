f64635b3f1fe3f348b85b93a599ab2e9356b5f5e
Felipe Ante <Felpe.Ante@gmail.com>
2026-01-28 09:35:19 -0500

    feat: se crea el primer programa de saludo y se coemnta el codigo

---
+++ Module-01/Class-01/MiPrimerSaludo.java
@@ -0,0 +1,9 @@
+public class MiPrimerSaludo {
+    public static void main(String[] args) 
+    {
+        //este es un programa que muestra en consola un saludo //
+        System.out.println("Hola, soy Andres Felipe Ante y este es mi primer programa en java!");
+        System.err.println("Estoy muy EMOCIONADO de aprender programacion con java.");
+    }
+    
+}
